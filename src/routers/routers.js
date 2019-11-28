811b9b045a996dbf7ea4a630e5b56cd3339be556
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2019-11-28 14:38:10 -0600

    Skeleton for the frontend Soccer Project

---
+++ src/routers/routers.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "../containers/header.js";
+import { BrowseRouter as Router } from "react-router-dom";
+
+class SideBarMenuRouters extends Component {
+  constructor() {
+    this.state = { menuOpen: true };
+  }
+
+  render(){
+    
+  }
+}
